aef1b7d6dec3c82a4ca149fbc263869e8501528e
keremsabirli <keremsabirli15@gmail.com>
2025-02-19 06:56:30 +0300

    tailwind bug reproduce application

---
+++ src/app/app-button/app-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-button',
+  imports: [],
+  templateUrl: './app-button.component.html',
+  styleUrl: './app-button.component.css'
+})
+export class AppButtonComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonComponent } from 'lib'
+import { AppButtonComponent } from './app-button/app-button.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonComponent, AppButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
